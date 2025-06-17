c43ad8434e53e6f6698882e2c5a9d2d5d5d4d12a
Mohammed Numaan <138296610+mohammednumaan@users.noreply.github.com>
2025-06-17 15:39:34 +0530

    feat: added new ui changes to profile component due to api changes for that route.

--- src/ui/Profile/Profile.tsx
+++ src/ui/Profile/Profile.tsx
@@ -1,12 +1,14 @@
 import toast from "react-hot-toast";
 import useFetch from "../../core/hooks/fetch.hook";
-import { IProfileJots } from "../../core/types/jot/jotGroup.types";
 import { useAuth } from "../../core/context/auth.context";
 import { formatDistanceToNowStrict } from "date-fns";
+import { Link } from "react-router";
+import Button from "../Form/components/Button";
+import { AllJotsResponse } from "../../core/types/jot/jots";
 
 export default function Profile() {
   const { username } = useAuth();
-  const { data, loading, error } = useFetch<IProfileJots>(
+  const { data, loading, error } = useFetch<AllJotsResponse>(
     `user/${username}/jots`
   );
 
@@ -14,43 +16,99 @@ export default function Profile() {
     error.map((err) => toast(err));
   }
 
+  if (error) {
+    error.map((err) => toast(err));
+  }
+
   return (
     <div>
-      {/* this is the user's jots list */}
-      <div>
-        <p className="text-2xl text-[#543A8B] font-bold mb-5">All Jots</p>
-        <div className="flex flex-col gap-10">
-          {data?.jots &&
-            data.jots.length > 0 &&
-            data.jots.map((jot) => (
-              <div className="w-full flex flex-col p-5 bg-[#080808] rounded-tr-md rounded-tl-md">
-                <div className="flex justify-between items-center">
-                  {/* this is the right header for the jot which contains the jot's 
-                        description and the updated date 
-                  */}
-                  <div className="flex flex-col items-start justify-center">
-                    <p className="text-[#543A8B]">{jot.description}</p>
-                    <p className="text-sm opacity-[0.6]">
-                      Updated {formatDistanceToNowStrict(jot.updatedAt)} ago
-                    </p>
-                  </div>
+      {error && (
+        <h1 className="h-[80vh] flex justify-center items-center">
+          Could not fetch jots, please try again later.
+        </h1>
+      )}
 
-                  {/* this is the left header of the jot which contains the number of files it contains */}
+      {/* TODO: refactor this since its the same code as the one in discover with
+         some components removed (like the editor component)
+      */}
+      {data?.jots.length === 0 && (
+        <div className="h-[80vh] w-[100%] flex flex-col justify-center items-center gap-5">
+          <p className=" text-4xl flex justify-center items-center">
+            No Jots found. Start by creating one.
+          </p>
+          <Link className="w-[57%]" to="/create">
+            <Button imagePath="/public/icons/add_black.svg">Create Jot</Button>
+          </Link>
+        </div>
+      )}
+      <div className="flex justify-center items-center flex-col gap-5">
+        <p className="text-2xl text-[#543A8B]">All Jots</p>
+
+        {!loading &&
+          !error &&
+          data?.jots &&
+          data.jots.length > 0 &&
+          data.jots.map((jot) => (
+            <div
+              key={jot.id}
+              className="flex gap-5 flex-col w-full bg-[#080808] p-4 rounded-md"
+            >
+              {/* this is the jot's header metadata (name, number of files, etc) */}
+              <div className="flex justify-between">
+                {/* this is the left side of the header metadata */}
+                <div className="flex justify-center items-start flex-col">
+                  <div className="flex gap-2">
+                    <Link
+                      className="hover:underline"
+                      to={`/profile/${jot.owner.id}`}
+                    >
+                      <p className="text-xl text-[#543A8B]">{jot.owner.name}</p>
+                    </Link>
+                    <p className="text-xl text-[#543A8B]">/</p>
+                    <Link
+                      className="hover:underline"
+                      to={`/${jot.owner.name}/${jot.jotGroup.id}`}
+                    >
+                      <p className="text-xl text-[#543A8B]">
+                        {jot.name + "." + jot.extension}
+                      </p>
+                    </Link>
+                  </div>
+                  <p className="text-sm opacity-[0.6]">
+                    Updated {formatDistanceToNowStrict(jot.updatedAt)} ago
+                  </p>
+                  <p className="text-sm opacity-[0.6]">{jot.description}</p>
+                </div>
+
+                {/* this is the right side of the header metadata */}
+                <div>
                   <div className="flex gap-2">
                     <img
                       src={"/public/icons/code_block.svg"}
                       alt="code block icon"
                     />
                     <p className="text-sm opacity-[0.6]">
-                      {jot.totalFiles} file
-                      {jot.totalFiles > 1 ? "s" : ""}
+                      {jot.jotGroup.totalFiles} file
+                      {jot.jotGroup.totalFiles > 1 ? "s" : ""}
                     </p>
                   </div>
                 </div>
               </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
+
+      {/* {loading && !error && data?.pagination && (
+        <div className="mt-5 mb-5">
+          <Pagination
+            page={currentPage}
+            totalPages={data.pagination.totalPages}
+            goToNextPage={goToNextPage}
+            goToPage={goToPage}
+            goToPrevPage={goToPrevPage}
+          />
+        </div>
+      )} */}
     </div>
   );
 }
